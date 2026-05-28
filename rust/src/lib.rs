ee3c459f0497be1f4b5bb8a9731bbb8e9f47e9bd
Aaron Askew <aaronaskew@gmail.com>
2026-05-28 07:53:03 -0500

    added Player init and physics_process functions. changed type in godot project

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,36 @@
-use godot::prelude::*;
+use godot::{
+    classes::{ISprite2D, Sprite2D},
+    prelude::*,
+};
 
 struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    speed: f64,
+    angular_speed: f64,
+    base: Base<Sprite2D>,
+}
+
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Hello World!");
+
+        Self {
+            speed: 400.0,
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
+    }
+
+    fn physics_process(&mut self, delta:f64) {
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
+    }
+}
+
